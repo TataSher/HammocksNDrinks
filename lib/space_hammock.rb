a1e1e49140384a398ad565b422fb5db9fdf4861c
Sha <cspoppuppy@hotmail.com>
2021-03-29 17:33:00 +0100

    add SpaceHammocks class

--- lib/space_hammock.rb
+++ lib/space_hammock.rb
@@ -1,3 +1,6 @@
 class SpaceHammock
-
+  def self.create(name, description, price_per_night, user_id)
+    sql = "INSERT INTO hammocks (name, description, price_per_night, user_id) VALUES ('#{name}', '#{description}', #{price_per_night}, #{user_id})"
+    PG.exec(sql)
+  end
 end
---
+++ spec/units/space_hammock_spec.rb
@@ -0,0 +1,13 @@
+require 'space_hammock'
+
+describe SpaceHammock do
+  describe '.create' do
+    it 'creates a space hammock' do
+      sql = "INSERT INTO hammocks (name, description, price_per_night, user_id) VALUES ('test_name', 'test_description', 50, 1)"
+      expect(PG).to receive(:exec).with(sql)
+      SpaceHammock.create('test_name', 'test_description', 50, 1)
+    end
+  end
+
+
+end
