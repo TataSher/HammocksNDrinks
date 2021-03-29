7c43d6bca5ea656549444303743cec78e7f883a0
Natalia_Sherchenkova <tataterer@gmail.com>
2021-03-29 17:29:52 +0100

    Feature test updated to print new instance of SpaceHammock

--- makers_hnd_web_app.rb
+++ makers_hnd_web_app.rb
@@ -1,6 +1,7 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
 require 'sinatra/flash'
+require './lib/SpaceHammock.rb'
 
 class MakersHnDWebApp < Sinatra::Base
   enable :sessions, :method_override
@@ -16,6 +17,7 @@ class MakersHnDWebApp < Sinatra::Base
   end
 
   get '/hammocks_listings' do
+    @hammock = SpaceHammock.new('Sharkie', 'coolest hammock', '1000', 'Lou') 
     erb :index
   end
   
--- spec/features/list_all_spaces_spec.rb
+++ spec/features/list_all_spaces_spec.rb
@@ -3,6 +3,9 @@ require './makers_hnd_web_app.rb'
 feature 'list all space hammocks' do
     scenario 'list one space hammock' do
       visit '/hammocks_listings'
-      expect(page).to have_content('Best Space Hammock EVER!')
+      expect(page).to have_content('Sharkie')
+      expect(page).to have_content('coolest hammock')
+      expect(page).to have_content('1000')
+      expect(page).to have_content('Lou')
     end
 end
